7aa904c2031bdd7bf865c32078faddecaea982f9
madrara256 <herbertichama@outlook.com>
2020-09-03 13:20:46 +0300

    Initial commit

---
+++ purchase_requisition/models/__init__.py
@@ -0,0 +1,3 @@
+# Part of System. See LICENSE file for full copyright and licensing details.
+
+from . import purchase_requisition
---
+++ purchase_requisition/tests/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Part of System. See LICENSE file for full copyright and licensing details.
+
+from . import test_purchase_requisition
