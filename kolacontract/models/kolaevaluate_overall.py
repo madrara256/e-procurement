26f20736c20dd261601e3776a166a6812842e993
madrara256 <herbertichama@outlook.com>
2020-09-10 09:18:08 +0300

    improved evaluation query to calculate average

--- kolacontract/models/kolaevaluate_overall.py
+++ kolacontract/models/kolaevaluate_overall.py
@@ -11,10 +11,55 @@ from werkzeug import url_encode
 class KolaevaluateOverall(models.Model):
 	_name = 'kola.evaluate.overall'
 	_description = 'Kola Evaluate Overall Score'
-	_inherit = ['mail.thread']
+	_auto = False
+	_rec_name = 'supplier_id'
 
-	name = fields.Char(string='Reference', default='New', require=True, track_visibility='onchange')
-	contract_id = fields.Many2one('kola.contract', string='Contract', track_visibility='onchange')
-	supplier_id = fields.Many2one('res.partner',string='Supplier')
-	department_id = fields.Many2one('hr.department', related='contract_id.department_id', string='Department')
-	color = fields.Integer(string='Index')
+
+	supplier_id = fields.Many2one('res.partner',string='Supplier', readonly=True)
+	department_id = fields.Many2one('hr.department', readonly=True)
+	color = fields.Integer(string='Index', readonly=True)
+	surveys_submitted = fields.Integer(string='Surveys', readonly=True)
+	service_provider_average_score = fields.Float(string='Average Score', digits=(12,1))
+	supplier_average_score = fields.Float(string='Average Score', digits=(12,1))
+	evaluation = fields.Char(string='Evaluate for?')
+
+	def _select(self):
+		return """ 
+			SELECT 
+				min(ke.id) as id,
+				ke.supplier_id,
+				ke.department_id,
+				ke.evaluation,
+				count(supplier_id) as surveys_submitted,
+				(sum(service_provider_score)/
+				count(supplier_id)
+				) AS service_provider_average_score,
+				(sum(supplier_score)/
+					count(supplier_id)
+				) AS supplier_average_score
+				
+		"""
+
+	def _from(self):
+		return """
+			FROM 
+				kolacontract_evaluate as ke
+		"""
+
+	def _groupby(self):
+		return """
+			GROUP BY
+				ke.supplier_id,
+				ke.department_id,
+				ke.evaluation
+		"""
+	def init(self):
+		tools.drop_view_if_exists(self._cr, self._table)
+		self._cr.execute("""
+			CREATE OR REPLACE VIEW %s AS (
+				%s
+				%s
+				%s
+			)
+			""" % (self._table, self._select(), self._from(), self._groupby()))
+
