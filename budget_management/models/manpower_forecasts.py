b9660586d4a61934f32700d03f2c5726e34df0e5
madrara256 <herbertichama@outlook.com>
2020-09-29 07:23:25 +0300

    remodeling the manpower forecasts

--- budget_management/models/manpower_forecasts.py
+++ budget_management/models/manpower_forecasts.py
@@ -12,32 +12,20 @@ class ManPowerPlan(models.Model):
 	_description = 'manpower forecasts'
 	_rec_name='team_id'
 
+	name = fields.Char(string='Reference', default='New', required=True)
 	team_id = fields.Many2one('budget.team',string='Branch',)
-	months = fields.Selection([
-								('jan', 'January'),
-								('feb', 'February'),
-								('mar', 'March'),
-								('apr','April'),
-								('may', 'May'),
-								('jun', 'June'),
-								('jul', 'July'),
-								('aug', 'August'),
-								('sep','September'),
-								('oct','October'),
-								('nov', 'November'),
-								('dec', 'December')],string='Months',
-								group_expand='_expand_states',)
-	def _expand_states(self, states, domain, order):
-		return [key for key, val in type(self).months.selection]
-
-	job_id = fields.Many2one('hr.job', string='Position')
-	current_number = fields.Integer(string='Current No.')
-	proposed_number = fields.Integer(string='Proposed')
-	difference = fields.Integer(string='Variance')
+	projection_period_id = fields.One2many('manpower.projection.period', 'man_projection_id', 
+		'Projections Parameters', track_visibility='onchange')
+	active = fields.Boolean(string='Active', default=True)
 	color = fields.Integer(string='Index')
+	date_from = fields.Date(string='Period')
+	date_to = fields.Date(string='End Date')
+
 
 	@api.model
 	def create(self,values):
+		if values.get('name', 'New') == 'New':
+			values['name'] = self.env['ir.sequence'].next_by_code('manpower.sequence.code') or 'New'
 		rec = super(ManPowerPlan, self).create(values)
 		return rec
 
@@ -55,5 +43,30 @@ class ManPowerPlan(models.Model):
 		raise UserError(_('Plan can not be duplicated, please contact system Administrator'))
 
 
+class StaffingProjectionPeriod(models.Model):
+		_name = 'manpower.projection.period'
+		_description = 'Projection Period'
 
+		name = fields.Selection(
+			[
+				('jan', 'January'),
+				('feb', 'February'),
+				('mar', 'March'),
+				('apr', 'April'),
+				('may', 'May'),
+				('jun', 'June'),
+				('jul', 'July'),
+				('aug', 'August'),
+				('sep', 'September'),
+				('oct', 'October'),
+				('nov', 'November'),
+				('dec', 'December')
+				],string='Period', default='jan')
+		man_projection_id = fields.Many2one('manpower.forecasts', string='Projection Reference')
+
+		department_id = fields.Many2one('hr.department', string='Department')
+		job_id = fields.Many2one('hr.job', string='Position')
+		current_number = fields.Integer(string='Current No.')
+		proposed_number = fields.Integer(string='Proposed')
+		difference = fields.Integer(string='Variance')
 
