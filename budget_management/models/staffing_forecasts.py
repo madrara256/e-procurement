8d215956307448c0868300dd0e761dac3db7d6fd
madrara256 <herbertichama@outlook.com>
2020-09-29 07:24:00 +0300

    remodeling the staffing forecasts

--- budget_management/models/staffing_forecasts.py
+++ budget_management/models/staffing_forecasts.py
@@ -9,37 +9,22 @@ from odoo.exceptions import UserError, AccessError,ValidationError
 class StaffForecasts(models.Model):
 	_name = 'staff.forecasts'
 	_description = 'Staffing Forecasts'
-	_rec_name = 'department_id'
+	_rec_name = 'team_id'
 	_inherit = ['mail.thread', 'mail.activity.mixin']
 
+	name = fields.Char(string='Reference', default='New', required=True)
 	team_id = fields.Many2one('budget.team',string='Branch',)
-	months = fields.Selection([
-								('jan', 'January'),
-								('feb', 'February'),
-								('mar', 'March'),
-								('apr','April'),
-								('may', 'May'),
-								('jun', 'June'),
-								('jul', 'July'),
-								('aug', 'August'),
-								('sep','September'),
-								('oct','October'),
-								('nov', 'November'),
-								('dec', 'December')],string='Months',
-								group_expand='_expand_states',)
-	def _expand_states(self, states, domain, order):
-		return [key for key, val in type(self).months.selection]
-
-	department_id = fields.Many2one('hr.department', string='Department')
-	job_id = fields.Many2one('hr.job', string='Position')
-	current_number = fields.Integer(string='Current No.')
-	proposed_number = fields.Integer(string='Proposed')
-	difference = fields.Integer(string='Variance')
+	projection_period_id = fields.One2many('staffing.projection.period', 'staffing_projection_id', 
+		'Projections Parameters', track_visibility='onchange')
+	active = fields.Boolean(string='Active', default=True)
 	color = fields.Integer(string='Index')
-
+	date_from = fields.Date(string='Period')
+	date_to = fields.Date(string='End Date')
 
 	@api.model
 	def create(self, values):
+		if values.get('name', 'New') == 'New':
+			values['name'] = self.env['ir.sequence'].next_by_code('staffing.sequence.code') or 'New'
 		staffing = super(StaffForecasts, self).create(values)
 		return staffing
 
@@ -55,5 +40,34 @@ class StaffForecasts(models.Model):
 
 	def copy_data(self, context=None):
 		raise UserError(_('Staffing projection can not be duplicated...t'))
+
+
+class StaffingProjectionPeriod(models.Model):
+		_name = 'staffing.projection.period'
+		_description = 'Projection Period'
+
+		name = fields.Selection(
+			[
+				('jan', 'January'),
+				('feb', 'February'),
+				('mar', 'March'),
+				('apr', 'April'),
+				('may', 'May'),
+				('jun', 'June'),
+				('jul', 'July'),
+				('aug', 'August'),
+				('sep', 'September'),
+				('oct', 'October'),
+				('nov', 'November'),
+				('dec', 'December')
+				],string='Period', default='jan')
+		staffing_projection_id = fields.Many2one('staff.forecasts', string='Projection Reference')
+
+		department_id = fields.Many2one('hr.department', string='Department')
+		job_id = fields.Many2one('hr.job', string='Position')
+		current_number = fields.Integer(string='Current No.')
+		proposed_number = fields.Integer(string='Proposed')
+		difference = fields.Integer(string='Variance')
+
 
 
