17e3143dcb6ce44be40d72765728f56bcd4f0d2d
madrara256 <herbertichama@outlook.com>
2020-10-21 14:17:48 +0300

    function logic for wizard

--- budget_management/wizard/budget_reject_reason.py
+++ budget_management/wizard/budget_reject_reason.py
@@ -3,4 +3,13 @@ from odoo import api,fields,models,_
 class BudgetRefuseReason(models.TransientModel):
 	_name = 'budget.refuse.reason'
 
-	
+	budget_refuse_reason = fields.Html(string='Refuse Reason', required = True)
+
+	def refuse_reason(self):
+		if self.env.context.get('active_model') =='budget_management':
+			active_model_id = self.env.context.get('active_id')
+			kolarequest_obj = self.env['budget.management'].search([('id', '=', active_model_id)])
+			if kolarequest_obj:
+				kolarequest_obj.write({'budget_refuse_reason':self.budget_refuse_reason})
+				kolarequest_obj.action_reject_proposal()
+
