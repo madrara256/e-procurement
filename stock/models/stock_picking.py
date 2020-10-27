702773b9ff51bbfa51557a14402127404d9c7b3c
madrara256 <herbertichama@outlook.com>
2020-10-27 11:10:52 +0300

    altered required fields

--- stock/models/stock_picking.py
+++ stock/models/stock_picking.py
@@ -200,13 +200,16 @@ class Picking(models.Model):
 		('done', 'Done'),
 		('cancel', 'Cancelled'),
 	], string='Status', compute='_compute_state',
-		copy=False, index=True, readonly=True, store=True, track_visibility='onchange',
+		copy=False, index=True, readonly=True, store=True, track_visibility='onchange', group_expand='_expand_states',
 		help=" * Draft: not confirmed yet and will not be scheduled until confirmed.\n"
 			 " * Waiting Another Operation: waiting for another move to proceed before it becomes automatically available (e.g. in Make-To-Order flows).\n"
 			 " * Waiting: if it is not ready to be sent because the required products could not be reserved.\n"
 			 " * Ready: products are reserved and ready to be sent. If the shipping policy is 'As soon as possible' this happens as soon as anything is reserved.\n"
 			 " * Done: has been processed, can't be modified or cancelled anymore.\n"
 			 " * Cancelled: has been cancelled, can't be confirmed anymore.")
+
+	def _expand_states(self, states, domain, order):
+		return [key for key, val in type(self).state.selection]
 
 	group_id = fields.Many2one(
 		'procurement.group', 'Procurement Group',
